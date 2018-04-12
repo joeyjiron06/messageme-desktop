be7cd4e6907850d479d8c4d05584e9dcb5e92699
Joey Jiron <joey.jiron@am.sony.com>
2018-04-12 11:58:24 -0700

    fixed input issue

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
 import registerServiceWorker from './registerServiceWorker';
-import packageJson from './package.alias.json';
-console.log('pagage', packageJson);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
